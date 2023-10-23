0af337ce33fef14fdbd026a353aca350aff85407
klaudek123 <klaudiusz1tai@gmail.com>
2023-10-23 10:32:41 +0200

    Update Program.java

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -2,7 +2,7 @@ public class Main {
   int x = 5;
   int y = 10;
   int z = 15;
-
+  int xd = 0;
   public static void main(String[] args) {
     Main myObj = new Main();
     System.out.println(myObj.x);
